e7e4f7a908ff026908e8aafab6fc6335cbc4819d
L8D <tenorbiel@gmail.com>
2014-10-27 01:14:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var Future = require('data.future');
+
+module.exports = function makeRouter(route, handler, parent) {
+  var router = function routeHandler(oreq, ores, onext) {
+    if (!onext) {
+      onext = function next(req, res) {
+        return Future.of(res);
+      };
+    }
+
+    if (!parent) {
+      return onext(oreq, ores);
+    }
+
+    return parent(oreq, ores, function next(req, res) {
+      var path = req.get('path');
+
+      // skip if route doesn't match
+      if (path.toLowerCase().substr(0, route.length) !== route.toLowerCase()) {
+        return onext(req, res);
+      }
+
+      // skip if route match does not border '/', '.' or end
+      var c = path[route.length];
+      if (c !== undefined && c !== '/' && c !== '.') {
+        return onext(req, res);
+      }
+
+      // trim off the part of the url that matches the route
+      if (route.length !== 0 && route !== '/') {
+        var removed = route;
+        var fqreq = req
+          .update('url', sliceAtRemoved)
+          .update('path', sliceAtRemoved);
+
+        return handler(fqreq, res, function next(nreq, nres) {
+          nreq = nreq.update('url', insertRemoved);
+          nreq = nreq.update('path', insertRemoved);
+
+          return onext(nreq, nreq);
+        });
+      }
+
+      return handler(req, res, onext);
+
+      function sliceAtRemoved(path) {
+        path = path.slice(removed.length);
+
+        if (path[0] === '/') {
+          return path;
+        } else {
+          return '/' + path;
+        }
+      }
+
+      function insertRemoved(path) {
+        if (removed[removed.length] === '/') {
+          return removed + path.slice(1);
+        } else {
+          return removed + path;
+        }
+      }
+    });
+  };
+
+  router.use = function(route, fn) {
+    if (typeof route !== 'string') {
+      fn = route;
+      route = '/';
+    }
+
+    if (route[route.length - 1] === '/') {
+      route = route.slice(0, -1);
+    }
+
+    return makeRouter(route, fn, this);
+  };
+
+  return router;
+};
